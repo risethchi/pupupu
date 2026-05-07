9b07beb988f6d74108c300e1f791977e8458fbb8
Margo Lazareva <87071482+risethchi@users.noreply.github.com>
2026-05-08 01:42:46 +0400

    move template names into consts (#1)

---
+++ config.py
@@ -0,0 +1,12 @@
+REGISTER_VIEW = 'users/register.html'
+LOGIN_VIEW = 'users/login.html'
+PARTICIPANTS_VIEW = 'users/participants.html'
+USER_DETAIL_VIEW = 'users/user-details.html'
+USER_EDIT_VIEW = 'users/edit_profile.html'
+USER_CHANGE_PASSWORD_VIEW = 'users/change_password.html'
+
+PROJECT_LIST_VIEW = 'projects/project_list.html'
+PROJECT_FAVORITE_VIEW = 'projects/favorite_projects.html'
+PROJECT_CREATE_VIEW = 'projects/create_project.html'
+PROJECT_DETAIL_VIEW = 'projects/project_detail.html'
+
